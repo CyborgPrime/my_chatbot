f0c407046c9e5b64f8b559b8ba5761f5e9491db3
CyborgPrime <98671608+CyborgPrime@users.noreply.github.com>
2023-10-20 20:45:42 -0600

    update

--- app.py
+++ app.py
@@ -1,42 +1,6 @@
-from flask import Flask, render_template, request
-import openai
-import os
-
-openai.api_key = os.environ["OPENAI_API_KEY"]
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def chatbot():
-    if request.method == "POST":
-        prompt = request.form.get("question")
-
-        if prompt.lower() == "quit":
-            return render_template("chatbot.html", response="You have quit the conversation.")
-
-        result = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a game master bot. You can create compelling adventures and present them through a text adventure simulator interface."
-                },
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
-        )
-
-        response = result.choices[0].message.content
-
-        return render_template("chatbot.html", response=response)
-
-    return render_template("chatbot.html", response=None)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def hello_world():
+    return '<h1 style="color: white;">Hello, Cyber World!</h1>'
