a8aa3204844216dc0dbd4c76e1cfa09b120e6056
CyborgPrime <98671608+CyborgPrime@users.noreply.github.com>
2023-10-20 21:26:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
+# app.py (main app)
+
 from flask import Flask
-import subprocess
+from my_basic_web_chatbot import app as chatbot_app  # Import the chatbot app
 
 app = Flask(__name__)
 
+# Mount the chatbot app at the root URL ("/")
+app.register_blueprint(chatbot_app, url_prefix='/')
+
 @app.route('/')
-def run_chatbot():
-    # Use subprocess to run the "my_basic_web_chatbot.py" script
-    try:
-        subprocess.run(["python", "my_basic_web_chatbot.py"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        return '<h1 style="color: white;">Hello, Cyber World!</h1>'
-    except subprocess.CalledProcessError as e:
-        # Handle any errors that occur when running the script
-        error_message = f"An error occurred: {e.stderr.decode()}"
-        return f'<p style="color: red;">{error_message}</p>'
+def main_route():
+    return 'Hello from Main App!'
 
 if __name__ == '__main__':
     app.run(debug=True)
