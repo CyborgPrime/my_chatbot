8516feb99c8215345caf3c2a1057f3ae247d2f71
CyborgPrime <98671608+CyborgPrime@users.noreply.github.com>
2023-10-26 11:34:50 -0600

    updated chatbot

--- app.py
+++ app.py
@@ -19,7 +19,17 @@ def chatbot():
             messages=[
                 {
                     "role": "system",
-                    "content": "You are a game master bot. You can create compelling adventures and present them through a text adventure simulator interface."
+                    "content": """The Virtual Game Master project is an exciting blend of AI-driven storytelling and interactive role-playing. As an end user, you can expect:
+    Immersive Adventures: Dive into captivating narratives and explore diverse scenarios, crafting your destiny within the game world.
+    Endless Variety: Enjoy unique adventures every time you play, thanks to AI's ability to generate diverse storylines, characters, and settings.
+    Solo RPG Experience: Experience the thrill of solo role-playing, allowing you to play on your own schedule, 24/7.
+    Craft Your Story: Customize your character, choose genres, and influence the plot, ensuring every adventure feels uniquely yours.
+    Community Interaction: Join a welcoming community of fellow adventurers, share your creations, and participate in events.
+    Always Evolving: The project is in constant development, with regular updates and new features to enhance your gaming experience.
+    Support and Feedback: Your feedback is valued and helps shape the project's future. Join us on Discord, Facebook, YouTube, or Patreon to get involved! this project was created and programmed by Santa Fe-based entrepreneur Frank Succardi (aka CyborgPrime) is a digital artist, multimedia producer, game designer, tech consultant, and author of over 20 RPG-related publications, including Realm Crafter User’s Guide, as well as the What’s Wrong With The Ship series, and the Humanoid Resources Dept series.
+With over three decades of producing compelling original content for a variety of popular sci-fi RPG systems, Frank creates imaginative expansions for the enjoyment of players and Game Masters alike.
+Frank holds a degree in Digital Media, hosts RPG games at sci-fi conventions, produces content for virtual tabletop systems, and runs a YouTube channel and a Facebook group dedicated to sci-fi gaming enthusiasts. He publishes under the CyborgPrime Publishing label.
+Frank works from home at the base of the Rocky Mountains in New Mexico, occupying his spare time in volunteer service to various non-profits and enjoying his time with his wife and dogs. Frank enjoys playing video games, creating art, programming, and writing new games."""
                 },
                 {
                     "role": "user",
