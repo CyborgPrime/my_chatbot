296e2996ac9dad3fc669bf3c07cda454e7c2a35d
CyborgPrime <98671608+CyborgPrime@users.noreply.github.com>
2023-10-29 16:37:20 -0600

    tryingf flask session manager

--- storyteller_web_chatbot.py
+++ storyteller_web_chatbot.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, session
-from flask_session import Session
 import os
-import redis
 
-# Import module dependencies
+# import module dependencies
 from langchain import LLMChain, PromptTemplate
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chat_models import ChatOpenAI
@@ -17,15 +15,6 @@ app = Flask(__name__)
 # Initialize the Flask session with a secret key
 app.secret_key = os.environ.get("FLASK_SECRET_KEY")
 
-# Configure Flask-Session to use Redis
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True  # This is recommended for better security
-app.config['SESSION_KEY_PREFIX'] = 'your_prefix_here'  # Change this to a unique prefix
-
-# Initialize the Flask-Session extension
-Session(app)
-
 # Define the AI model and parameters
 aiModel = "gpt-3.5-turbo"
 aiTemperature = 0.2
@@ -33,7 +22,7 @@ aiHistory = 20
 aiVerbosity = True
 
 # Define the game loop prompt
-gameLoopPrompt = "You are a text adventure game simulator taking the user through a story similar to The Adventures of Robin Hood but with the user as the protagonist. Avoid large blocks of text; be concise and take turns with the user. At each new location, be sure to specify the exits and the route the player took from the previous location. Do not preface your response with AI:"
+gameLoopPrompt = "You are a text adventure game simulator taking the user through a story similar to The Adventures of Robin Hood but with the user as the protagonist. Avoid large blocks of text; be concise and take turns with the user. At each new location, be sure to specify the exits and the route the player took frm the previous location. Do not preface your response with AI:"
 
 # Define the bot template for the game loop
 gameLoopTemplate = """
