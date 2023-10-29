a650ff1b1b47036e101c02a855b243a3691fd5cb
CyborgPrime <98671608+CyborgPrime@users.noreply.github.com>
2023-10-29 12:19:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
-# import module dependencies
-from langchain import LLMChain, PromptTemplate
-from langchain.memory import ConversationBufferWindowMemory
-from langchain.chat_models import ChatOpenAI
-import os
-import openai
-
-openai.api_key = os.environ["OPENAI_API_KEY"]
+# ... Import statements and setup code ...
 
 app = Flask(__name__)
 
-history = []
+# Initialize the session
+app.secret_key = 'your_secret_key_here'
 
-# Initialize the AI
-aiModel = "gpt-3.5-turbo"
-aiTemperature = 0.5
-aiHistory = 10
-aiVerbosity = False
-
-# Define the game loop prompt
-gameLoopPrompt = "You are a text adventure game simulator taking the user through a story similar to The Adventures of Robin Hood but with the user as the protagonist. Avoid large blocks of text; be concise and take turns with the user."
-
-# Define the bot template for the game loop
-gameLoopTemplate = """
-    {history}
-    {combined_input}
-"""
-
-# Load the variables into the gameLoopTemplate
-gameLoopPromptTemplate = PromptTemplate(
-    input_variables=["history", "combined_input"],
-    template=gameLoopTemplate
-)
-
-chatgpt_chain = LLMChain(
-    llm=ChatOpenAI(temperature=aiTemperature, model_name=aiModel),
-    prompt=gameLoopPromptTemplate,
-    verbose=aiVerbosity,
-    memory=ConversationBufferWindowMemory(k=aiHistory),
-)
-
+# Define a function to get or initialize the chat history for a session
+def get_or_initialize_history():
+    if 'history' not in session:
+        session['history'] = []
+    return session['history']
 
 @app.route('/', methods=['GET', 'POST'])
 def chat():
-    global history
+    history = get_or_initialize_history()
 
     if request.method == 'POST':
         user_input = request.form['user_input']
-        combined_input = f"System: {gameLoopPrompt}\nHuman: {user_input}"
+        system_input = "System: " + gameLoopPrompt
+        combined_input = f"{system_input}\nHuman: {user_input}"
         response = chatgpt_chain.predict(
             history="\n".join(history),
-            combined_input=combined_input
+            system_input=system_input,
+            user_input=user_input
         )
         history.append(f'User: {user_input}')
         history.append(response)
 
     return render_template('chat.html', history=history)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
