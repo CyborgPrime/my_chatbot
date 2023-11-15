d32691cfec567ad7ed3a6164904bde9852428166
CyborgPrime <98671608+CyborgPrime@users.noreply.github.com>
2023-11-15 09:33:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def display_env_var():
-    env_var = os.environ.get('FLASK_SECRET_KEY', 'Environment variable not set.')
-    return f'FLASK_SECRET_KEY: {env_var}'
+    env_var = os.environ.get('OPENAI_API_KEY', 'Environment variable not set.')
+    return f'OPENAI_API_KEY: {env_var}'
 
 if __name__ == '__main__':
     app.run(debug=True)
